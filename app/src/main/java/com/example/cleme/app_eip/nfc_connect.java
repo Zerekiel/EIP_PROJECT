7cfa7629c0aa3e2897ab5819449609ae5c99b106
Zerekiel <clement.deprost@epitech.eu>
2018-12-10 19:00:40 +0100

    change and repush

---
+++ app/src/main/java/com/example/cleme/app_eip/nfc_connect.java
@@ -0,0 +1,13 @@
+package com.example.cleme.app_eip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nfc_connect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_connect);
+    }
+}
