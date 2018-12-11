ed0e1eb9fa8e28c0bc379a8b4d8ec5f0dcabca3c
Joan <joan.falsimagne@epitech.eu>
2018-12-11 16:45:35 +0100

    add all pages + ling in work

---
+++ app/src/main/java/com/example/cleme/app_eip/Infos_medicales.java
@@ -0,0 +1,13 @@
+package com.example.cleme.app_eip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Infos_medicales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infos_medicales);
+    }
+}
--- app/src/main/java/com/example/cleme/app_eip/MainActivity.java
+++ app/src/main/java/com/example/cleme/app_eip/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.cleme.app_eip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void conection(View view)
+    {
+        Intent intent = new Intent(this, nfc_connect.class);
+        startActivity(intent);
     }
 }
