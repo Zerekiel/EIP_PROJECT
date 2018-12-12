9dc0c10c0a801c014cee1b2eaded39ef7894279f
Zerekiel <clement.deprost@epitech.eu>
2018-12-12 15:21:27 +0100

    final visu

--- app/src/main/java/com/example/cleme/app_eip/Infos_medicales.java
+++ app/src/main/java/com/example/cleme/app_eip/Infos_medicales.java
@@ -1,7 +1,9 @@
 package com.example.cleme.app_eip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Infos_medicales extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class Infos_medicales extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos_medicales);
     }
+
+    public void return_connect(View view)
+    {
+        Intent intent = new Intent(this, nfc_connect.class);
+        startActivity(intent);
+    }
 }
+
+
