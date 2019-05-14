ec1f4ab05a536e31f2de4c590df0f102d910076e
Floriane <floriane.roy@epitech.eu>
2019-05-14 21:54:30 +0200

    CI tests: fix Position literals first in String comparisons error

--- app/src/main/java/com/eipteam/healthsafe/MainActivity.java
+++ app/src/main/java/com/eipteam/healthsafe/MainActivity.java
@@ -31,9 +31,10 @@ public class MainActivity extends AppCompatActivity {
         EditText password = (EditText) findViewById(R.id.password);
         String id2 = password.getText().toString();
 
-        if (id1.equals("deprost") && id2.equals("password")) {
+        if ("deprost".equals(id1) && "password".equals(id2)) {
             startActivity(intent);
         }
+
         else {
             error_msg(getApplicationContext(), "WRONG ID OR PASSWD");
         }
