f7bedc9cbe6acf8ec76049af29bf0e27c64a834d
KosmosUniverse <ducret.thomas@gmail.com>
2019-05-28 17:36:51 +0200

    modify NdefMessageParser class to remove URI and SmartPoster management and let only Text.

--- App_mobile/app/src/main/java/com/eipteam/healthsafe/nfc_manager/parser/NdefMessageParser.java
+++ App_mobile/app/src/main/java/com/eipteam/healthsafe/nfc_manager/parser/NdefMessageParser.java
@@ -4,9 +4,7 @@ import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 
 import com.eipteam.healthsafe.nfc_manager.record.ParseNdefRecord;
-import com.eipteam.healthsafe.nfc_manager.record.SmartPoster;
 import com.eipteam.healthsafe.nfc_manager.record.TextRecord;
-import com.eipteam.healthsafe.nfc_manager.record.UriRecord;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +27,8 @@ public class NdefMessageParser {
         List<ParseNdefRecord> elements = new ArrayList<ParseNdefRecord>();
 
         for (final NdefRecord record : records) {
-            if (UriRecord.isUri(record))
-                elements.add(UriRecord.parse(record));
-            else if (TextRecord.isText(record))
+            if (TextRecord.isText(record))
                 elements.add(TextRecord.parse(record));
-            else if (SmartPoster.isPoster(record))
-                elements.add(SmartPoster.parse(record));
             else {
                 elements.add(new ParseNdefRecord() {
                     @Override
