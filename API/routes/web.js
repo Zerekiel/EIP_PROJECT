482a5a769d1393f5f0c7d56b1ce42b75a9b60893
denia_a <adil.denia@epitech.eu>
2019-06-03 14:40:54 +0200

    Correction for sprint EIP

--- API/routes/web.js
+++ API/routes/web.js
@@ -6,26 +6,28 @@ router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.json({
   message : "Ajoute un nouveau patient à la liste",
-  nom : "Dont ",//req.body.nom,
-  ville : "Panic Its ",//req.body.ville,
-  taille : "Organic", //req.body.taille,
+  nom : "Dont",//req.body.nom,
+  ville : "PanicIts",//req.body.ville,
+  taille : "170", //req.body.taille,
   methode : req.method
   })
 })
 
 .post('/', function(req, res, next) {
+     	console.log(req);
+     	console.log(req.query.nom);
         res.json({
-        message : "Ajoute un nouveau patient à la liste",
-        nom : req.body.nom,
-        ville : req.body.ville,
-        taille : req.body.taille,
+        message : req.body.nom,
+        nom : req.query.nom,
+        ville : req.query.ville,
+        taille : req.query.taille,
         methode : req.method
         })
 });
 
-// console.log(req.header('Content-Type'))
-//     console.log(req.body.username);
-//     console.log(req.body.password);
-//     console.log(req.body.email);
+ 	// console.log(req.header('Content-Type'))
+    	// console.log(req.body.username);
+     // console.log(req.body.password);
+     // console.log(req.body.email);
 
 module.exports = router;
