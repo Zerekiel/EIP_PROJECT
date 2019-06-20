84829e310bd6061c2c7f10426868ab7805d64f23
KosmosUniverse <ducret.thomas@gmail.com>
2019-06-20 12:15:37 +0200

    refactor NFC Scan

--- App_mobile/app/src/main/java/com/eipteam/healthsafe/NFCConnect.java
+++ App_mobile/app/src/main/java/com/eipteam/healthsafe/NFCConnect.java
@@ -3,22 +3,16 @@ package com.eipteam.healthsafe;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.MifareClassic;
-import android.nfc.tech.MifareUltralight;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import com.eipteam.healthsafe.nfc_manager.parser.NdefMessageParser;
-import com.eipteam.healthsafe.nfc_manager.record.ParseNdefRecord;
-
-import java.util.List;
 
 public class NFCConnect extends Activity {
 
@@ -48,12 +42,24 @@ public class NFCConnect extends Activity {
     protected void onResume() {
         super.onResume();
 
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
         if (nfcAdapter != null) {
             if (!nfcAdapter.isEnabled())
                 showWirelessSettings();
-
-            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
         }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+
+        try {
+            ndefDetected.addDataType("text/plain");
+        } catch (IntentFilter.MalformedMimeTypeException e) { }
+
+        IntentFilter[] exchangeFilters = new IntentFilter[] { ndefDetected };
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, exchangeFilters, null);
     }
 
     private void showWirelessSettings() {
@@ -73,19 +79,18 @@ public class NFCConnect extends Activity {
 
     @Override
     protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        resolveIntent(intent);
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            NdefMessage[] msgs = getNdefMessages(intent);
+
+            displayMsgs(msgs[0].getRecords()[0]);
+        }
     }
 
-    private void resolveIntent(Intent intent) {
-        String action = intent.getAction();
+    private NdefMessage[] getNdefMessages(Intent intent) {
+        NdefMessage[] msgs = null;
 
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction()) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
             Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-
-            NdefMessage[] msgs;
 
             if (rawMsgs != null) {
                 msgs = new NdefMessage[rawMsgs.length];
@@ -94,168 +99,28 @@ public class NFCConnect extends Activity {
                     msgs[i] = (NdefMessage) rawMsgs[i];
                 }
             } else {
-                byte[] empty = new byte[0];
-                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                byte[] payload = dumpTagData(tag).getBytes();
-                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
-                NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
-                msgs = new NdefMessage[] {msg};
-            }
+                byte[] empty = new byte[] {};
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] { record });
 
-            displayMsgs(msgs);
+                msgs = new NdefMessage[] { msg };
+            }
+        } else {
+            Log.d(intent.getAction(), "Unknown intent");
+            finish();
         }
+
+        return msgs;
     }
 
-    private void displayMsgs(NdefMessage[] msgs) {
-        if (msgs == null || msgs.length == 0)
-            return ;
-
-        StringBuilder builder = new StringBuilder();
-        List<ParseNdefRecord> records = NdefMessageParser.parse(msgs[0]);
-
-        final int size = records.size();
-
-        for (int i = 0; i < size; i++) {
-            ParseNdefRecord record = records.get(i);
-            String str = record.str();
-            builder.append(str).append("\n");
-        }
+    private void displayMsgs(NdefRecord raw) {
+        String msg = new String(raw.getPayload());
 
         Intent intent = new Intent(this, MedicalStats.class);
 
-        intent.putExtra("data", builder.toString());
+        intent.putExtra("data", msg);
 
         startActivity(intent);
-    }
-
-    private String dumpTagData(Tag tag) {
-        StringBuilder sb = new StringBuilder();
-        byte[] id = tag.getId();
-        sb.append("ID (hex): ").append(toHex(id)).append('\n');
-        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n');
-        sb.append("ID (dec): ").append(toDec(id)).append('\n');
-        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n');
-
-        String prefix = "android.nfc.tech.";
-        sb.append("Technologies: ");
-        for (String tech : tag.getTechList()) {
-            sb.append(tech.substring(prefix.length()));
-            sb.append(", ");
-        }
-
-        sb.delete(sb.length() - 2, sb.length());
-
-        for (String tech : tag.getTechList()) {
-            if (tech.equals(MifareClassic.class.getName())) {
-                sb.append('\n');
-                String type = "Unknown";
-
-                try {
-                    MifareClassic mifareTag = MifareClassic.get(tag);
-
-                    switch (mifareTag.getType()) {
-                        case MifareClassic.TYPE_CLASSIC:
-                            type = "Classic";
-                            break;
-                        case MifareClassic.TYPE_PLUS:
-                            type = "Plus";
-                            break;
-                        case MifareClassic.TYPE_PRO:
-                            type = "Pro";
-                            break;
-                        default:
-                            break;
-                    }
-                    sb.append("Mifare Classic type: ");
-                    sb.append(type);
-                    sb.append('\n');
-
-                    sb.append("Mifare size: ");
-                    sb.append(mifareTag.getSize() + " bytes");
-                    sb.append('\n');
-
-                    sb.append("Mifare sectors: ");
-                    sb.append(mifareTag.getSectorCount());
-                    sb.append('\n');
-
-                    sb.append("Mifare blocks: ");
-                    sb.append(mifareTag.getBlockCount());
-                } catch (Exception e) {
-                    sb.append("Mifare classic error: " + e.getMessage());
-                }
-            }
-
-            if (tech.equals(MifareUltralight.class.getName())) {
-                sb.append('\n');
-                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
-                String type = "Unknown";
-                switch (mifareUlTag.getType()) {
-                    case MifareUltralight.TYPE_ULTRALIGHT:
-                        type = "Ultralight";
-                        break;
-                    case MifareUltralight.TYPE_ULTRALIGHT_C:
-                        type = "Ultralight C";
-                        break;
-                    default:
-                        break;
-                }
-                sb.append("Mifare Ultralight type: ");
-                sb.append(type);
-            }
-        }
-
-        return sb.toString();
-    }
-
-    private String toHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = bytes.length - 1; i >= 0; --i) {
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-            if (i > 0) {
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-    }
-
-    private String toReversedHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bytes.length; ++i) {
-            if (i > 0) {
-                sb.append(" ");
-            }
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-        }
-        return sb.toString();
-    }
-
-    private long toDec(byte[] bytes) {
-        long result = 0;
-        long factor = 1;
-        for (int i = 0; i < bytes.length; ++i) {
-            long value = bytes[i] & 0xffl;
-            result += value * factor;
-            factor *= 256l;
-        }
-        return result;
-    }
-
-    private long toReversedDec(byte[] bytes) {
-        long result = 0;
-        long factor = 1;
-        for (int i = bytes.length - 1; i >= 0; --i) {
-            long value = bytes[i] & 0xffl;
-            result += value * factor;
-            factor *= 256l;
-        }
-        return result;
     }
 
     public void retour(View view)
--- App_mobile/app/src/main/java/com/eipteam/healthsafe/nfc_manager/record/ParseNdefRecord.java
+++
@@ -1,5 +0,0 @@
-package com.eipteam.healthsafe.nfc_manager.record;
-
-public interface ParseNdefRecord {
-    String str();
-}
