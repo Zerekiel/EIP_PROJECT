85e4cdba8ab49c3ae1c519611ecda54fa768c398
Zerekiel <clement.deprost@epitech.eu>
2019-10-18 12:53:11 +0200

    [RM*] Clean the repo by removing *

--- App_web/routes/info.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET info page */
-router.get('/', function(req, res, next){
-  res.render('info', { title: 'Express' });
-});
-
-module.exports = router;
