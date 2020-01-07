b1f41dfd65b1ca24dffa12c7ef7502dcb57c45b3
Paulo <paul.rouillard@epitech.eu>
2020-01-07 18:31:57 +0100

    info.js

--- App_web/routes/info.js
+++ App_web/routes/info.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET info page */
 router.get('/', function(req, res, next){
-  res.render('info', { title: 'Express' });
+  res.render('info', { title: 'Express', doc_name:'Nom_du_Docteur',
+      user_name: 'Paul'/*res.body.first_name + ' '+ stock.last*/, user_dateOfBirth: '04/03/1997', user_profession: 'student', user_address:'Paris', user_height: '1.78', user_weight :'85' + 'Kg',
+      user_blood_type: "O neg.", user_allergies: "pollens", user_surgeries: "Shoulder", user_broken_members: "None"});
 });
 
 module.exports = router;
