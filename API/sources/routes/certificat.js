7e007d484ea8d891e1fb781d49cdbfc158dcded9
Floriane <floriane.roy@epitech.eu>
2020-03-04 02:53:54 +0100

    Create root for certificate

--- API/sources/routes/certificat.js
+++ API/sources/routes/certificat.js
@@ -9,7 +9,8 @@ const jwt = require('jsonwebtoken')
 
 require('util').inspect.defaultOptions.depth = null
 
-router.get('/.well-known/acme-challenge/RsXY48ehzgQEZoRvk6NEBXAaJpLYvjQ3sKV8wAsOBYs', function(req, res, next) {
-   console.log("RsXY48ehzgQEZoRvk6NEBXAaJpLYvjQ3sKV8wAsOBYs.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ");
-   return res.status(200).send("RsXY48ehzgQEZoRvk6NEBXAaJpLYvjQ3sKV8wAsOBYs.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ").end();
+router.get('/.well-known/acme-challenge/zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q', function(req, res, next) {
+//   console.log("zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ");
+   return res.status(200).send("zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ").end();
 })
+//res.sendfile()
