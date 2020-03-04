059726c2e05014f706d6f98debf71e0ea06d96cf
Floriane <floriane.roy@epitech.eu>
2020-03-04 23:02:08 +0100

    Try res.sendfile

--- API/sources/routes/certificat.js
+++ API/sources/routes/certificat.js
@@ -6,6 +6,6 @@ require('util').inspect.defaultOptions.depth = null
 
 router.get('/.well-known/acme-challenge/zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q', function(req, res, next) {
 //   console.log("zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ");
-   return res.status(200).send("zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ").end();
+   return res.status(200).sendfile(path.join(__dirname, '.well-known/acme-challenge', 'zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q')).end();
 })
 //res.sendfile()
