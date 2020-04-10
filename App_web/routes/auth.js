16bb29d6ea9261dce72c18248614c0c231881542
Zerekiel <clement.deprost@epitech.eu>
2020-04-10 22:13:36 +0200

    finally fix the motherf***ing routes

--- App_web/routes/auth.js
+++ App_web/routes/auth.js
@@ -1,31 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var _username = undefined;
-var _password = undefined;
+
 
 /* GET login page. */
 router.get('/', function(req, res) {
     res.render('auth', { title: 'Express' });
-    //console.log(res.body);
 });
+
+var options = {
+    url: 'https://healthsafe-api.herokuapp.com/api/connection',
+    method: 'POST',
+    json: {
+        userName: undefined,
+        password: undefined
+    },
+    feedJson: function(value1, value2) {
+        this.json.userName = value1;
+        this.json.password = value2;
+    }
+};
+
+var returnCode = {
+    code: 0,
+    getCode: function(_code) {
+        this.code = code;
+    }
+};
 
 router.post('/', function(req, res, next) {
-    _username = req.body.username;
-    _password = req.body.password;
-    request.post('https://healthsafe-api.herokuapp.com/api/connection', function() {
-        json: {
-            userName: _username,
-            password: _password
-        }
+    options.feedJson(req.body.username, req.body.password);
+    request(options, function(req, res) {
+        console.log(options.json.userName);
+        console.log(options.json.password);
+        console.log(res.body);
     });
+    //if (returnCode.code === 200) {
     res.redirect('/home');
     res.end();
+    //}
 });
-
-
-
-
-
 
 module.exports = router;
--- App_web/routes/info.js
+++ App_web/routes/info.js
@@ -1,37 +1,63 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request")
-var save_body = null;
+
+var infos = {
+    title: undefined,
+    doc_name: undefined,
+    user_name: undefined,
+    user_dateOfBirth: undefined,
+    user_profession: undefined,
+    user_address: undefined,
+    user_height: undefined,
+    user_weight: undefined,
+    user_blood_type: undefined,
+    user_allergies: undefined,
+    user_surgeries: undefined,
+    user_broken_members: undefined,
+    feedInfos: function(title, doc_name, user_name, user_dateOfBirth, user_profession,
+        user_address, user_height, user_weight, user_blood_type, user_allergies,
+        user_surgeries, user_broken_members) {
+        this.title = title;
+        this.doc_name = doc_name;
+        this.user_name = user_name;
+        this.user_dateOfBirth = user_dateOfBirth;
+        this.user_profession = user_profession;
+        this.user_address = user_address;
+        this.user_height = user_height;
+        this.user_weight = user_weight;
+        this.user_blood_type = user_blood_type;
+        this.user_allergies = user_allergies;
+        this.user_surgeries = user_user_surgeries;
+        this.user_broken_members = user_broken_members;
+    }
+};
+
+var options = {
+    url: 'https://healthsafe-api.herokuapp.com/api/stock',
+    method: 'GET'
+};
 
 /* GET info page */
 router.get('/', function(req, res, next) {
-
-    request.get('https://healthsafe-api.herokuapp.com/api/stock', function(req, res_api, next) {
-        save_body = res_api.body;
-        //console.log(save_body);
-
-        res.render('info', {
-            title: 'Express',
-            doc_name: 'Nom_du_Docteur',
-            user_name: 'Paul' /*res.body.first_name + ' '+ stock.last*/ ,
-            user_dateOfBirth: '04/03/1997',
-            user_profession: 'student',
-            user_address: 'Paris',
-            user_height: '1.78',
-            user_weight: '85' + 'Kg',
-            user_blood_type: "O neg.",
-            user_allergies: "pollens",
-            user_surgeries: "Shoulder",
-            user_broken_members: "None"
-        });
+    request(options, function(req, res, next) {
+        res.body;
+        console.log(res.body);
     });
-
-    //console.log(save_body);
+    res.render('info', {
+        title: infos.title,
+        doc_name: infos.doc_name,
+        user_name: 'Paul' /*res.body.first_name + ' '+ stock.last*/ ,
+        user_dateOfBirth: infos.user_dateOfBirth,
+        user_profession: infos.user_profession,
+        user_address: infos.user_address,
+        user_height: infos.user_height,
+        user_weight: infos.user_weight,
+        user_blood_type: infos.user_blood_type,
+        user_allergies: infos.user_allergies,
+        user_surgeries: infos.user_surgeries,
+        user_broken_members: infos.user_broken_members
+    });
 });
-
-/*function getUserData()
-{
-    request.get
-}*/
 
 module.exports = router;
