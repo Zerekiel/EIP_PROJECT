1781d47b8fd2234d82a2e9eba20c1140b5aa83f2
Zerekiel <clement.deprost@epitech.eu>
2020-04-10 17:07:51 +0200

    push a bugged version intended to be debugged
    (swithching computer so pushng to transit)

--- App_web/routes/auth.js
+++ App_web/routes/auth.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var _username = undefined;
+var _password = undefined;
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('auth', { title: 'Express' });
+    //console.log(res.body);
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    request.post('https://healthsafe-api.herokuapp.com/api/connection', {
+    _username = req.body.username;
+    _password = req.body.password;
+    request.post('https://healthsafe-api.herokuapp.com/api/connection', function() {
         json: {
-            userName: req.body.username,
-            password: req.body.password
+            userName: _username,
+            password: _password
         }
     });
-
-    request.get('https://healthsafe-api.herokuapp.com/api/connection', function(req, res) {
-        console.log(res.body);
-    });
-
     res.redirect('/home');
     res.end();
 });
+
+
+
+
+
 
 module.exports = router;
--- App_web/routes/info.js
+++ App_web/routes/info.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request")
+var save_body = null;
 
 /* GET info page */
-router.get('/', function(req, res, next){
-  res.render('info', { title: 'Express', doc_name:'Nom_du_Docteur',
-      user_name: 'Paul'/*res.body.first_name + ' '+ stock.last*/, user_dateOfBirth: '04/03/1997', user_profession: 'student', user_address:'Paris', user_height: '1.78', user_weight :'85' + 'Kg',
-      user_blood_type: "O neg.", user_allergies: "pollens", user_surgeries: "Shoulder", user_broken_members: "None"});
+router.get('/', function(req, res, next) {
+
+    request.get('https://healthsafe-api.herokuapp.com/api/stock', function(req, res_api, next) {
+        save_body = res_api.body;
+        //console.log(save_body);
+
+        res.render('info', {
+            title: 'Express',
+            doc_name: 'Nom_du_Docteur',
+            user_name: 'Paul' /*res.body.first_name + ' '+ stock.last*/ ,
+            user_dateOfBirth: '04/03/1997',
+            user_profession: 'student',
+            user_address: 'Paris',
+            user_height: '1.78',
+            user_weight: '85' + 'Kg',
+            user_blood_type: "O neg.",
+            user_allergies: "pollens",
+            user_surgeries: "Shoulder",
+            user_broken_members: "None"
+        });
+    });
+
+    //console.log(save_body);
 });
+
+/*function getUserData()
+{
+    request.get
+}*/
 
 module.exports = router;
