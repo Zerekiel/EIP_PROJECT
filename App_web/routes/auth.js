9649a3e1802058a0fabf9ae57b249ad866698fa2
Zerekiel <clement.deprost@epitech.eu>
2020-05-05 17:07:52 +0200

    add auth error check
    allow connection if auth data are correct

--- App_web/routes/auth.js
+++ App_web/routes/auth.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
+var request = require('request-promise');
 
 
 /* GET login page. */
@@ -9,36 +9,41 @@ router.get('/', function(req, res) {
 });
 
 var options = {
-    url: 'https://healthsafe-api.herokuapp.com/api/connection',
+    url: 'https://healthsafe-api.herokuapp.com/api/signin',
     method: 'POST',
     json: {
         userName: undefined,
         password: undefined
     },
-    feedJson: function(value1, value2) {
-        this.json.userName = value1;
-        this.json.password = value2;
+    feedJson: function(_username, _password) {
+        this.json.userName = _username;
+        this.json.password = _password;
     }
 };
 
 var returnCode = {
     code: 0,
     getCode: function(_code) {
-        this.code = code;
+        this.code = _code;
     }
 };
 
 router.post('/', function(req, res, next) {
     options.feedJson(req.body.username, req.body.password);
-    request(options, function(req, res) {
-        console.log(options.json.userName);
-        console.log(options.json.password);
-        console.log(res.body);
-    });
-    //if (returnCode.code === 200) {
-    res.redirect('/home');
-    res.end();
-    //}
+    request(options)
+        .catch(function(err) {
+            returnCode.getCode(err.statusCode);
+        });
+    if (returnCode.code === 200) {
+        console.log("all clear");
+
+        res.redirect('/home');
+        res.end();
+    } else {
+        console.log("wrong id");
+        res.redirect('/');
+        res.end();
+    }
 });
 
 module.exports = router;
