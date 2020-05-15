a1dfb57684f731a6045be38123b00763a754b793
Floriane <floriane.roy@epitech.eu>
2020-05-15 02:53:18 +0200

    Create HTTPS certificate

--- API/sources/routes/certificate.js
+++ API/sources/routes/certificate.js
@@ -4,9 +4,8 @@ var router = express.Router();
 
 require('util').inspect.defaultOptions.depth = null
 
-router.get('/.well-known/acme-challenge/API/.well-known/acme-challenge/Pm1PgxI_RpGKQ7DECHm8l3hbfI0AKH-LrESp4efiSIA', function(req, res, next) {
+router.get('.well-known/acme-challenge/rGhOaNjGW_jaIWrQ4q6uqnsfShohoPdsp52RRqx7oEI', function(req, res, next) {
 //   console.log("zACAJGF-vZC-pWVj3IaM0wRHSZ3UYjmfRm5WYfa_U2Q.9Oj-U5p-jg_9Fo0kLPuNzNmX4RwGL7UzpF0GXIP1UPQ");
-   return res.status(200).send("Pm1PgxI_RpGKQ7DECHm8l3hbfI0AKH-LrESp4efiSIA.UL87H8HPEIn3yS0CDCUKmabsrSJDf1d3PSpI4Y9nfxM
-")).end();
+   return res.status(200).send("rGhOaNjGW_jaIWrQ4q6uqnsfShohoPdsp52RRqx7oEI.UL87H8HPEIn3yS0CDCUKmabsrSJDf1d3PSpI4Y9nfxM")).end();
 })
 //res.sendfile()
