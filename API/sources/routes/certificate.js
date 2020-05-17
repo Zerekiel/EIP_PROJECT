64662565dd684e5e3ee80ead4a69178dbe95748a
Floriane <floriane.roy@epitech.eu>
2020-05-17 18:36:31 +0200

    [HTTPS]: TLS certificate

--- API/sources/routes/certificate.js
+++ API/sources/routes/certificate.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 require('util').inspect.defaultOptions.depth = null
 
-router.get('/.well-known/acme-challenge/CVijzRwNA8u8f1yKdPFvYVcxkA7LC8j77z-coCbmcnA', function(req, res, next) {
-  console.log("CVijzRwNA8u8f1yKdPFvYVcxkA7LC8j77z-coCbmcnA");
-   return res.status(200).send("CVijzRwNA8u8f1yKdPFvYVcxkA7LC8j77z-coCbmcnA.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g")).end();
-})
-
-router.get('/.well-known/acme-challenge/nXLZ1nhobw2fFL00031ju0_ai-QAla_7b142BkeBK3s', function(req, res, next) {
-  console.log("nXLZ1nhobw2fFL00031ju0_ai-QAla_7b142BkeBK3s.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g");
-   return res.status(200).send("nXLZ1nhobw2fFL00031ju0_ai-QAla_7b142BkeBK3s.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g")).end();
+router.get('/.well-known/acme-challenge/gMN31BrbXczasKzZgc5xAQJMy-pyCjJSZS6-GDH_DL0', function(req, res, next) {
+  console.log("gMN31BrbXczasKzZgc5xAQJMy-pyCjJSZS6-GDH_DL0.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g");
+   return res.status(200).send("gMN31BrbXczasKzZgc5xAQJMy-pyCjJSZS6-GDH_DL0.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g")).end();
 })
