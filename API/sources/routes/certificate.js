0ca0415a1701a161446589193a371066e361fd97
Floriane <floriane.roy@epitech.eu>
2020-05-17 22:12:38 +0200

    [HTTPS]: TLS certificate

--- API/sources/routes/certificate.js
+++ API/sources/routes/certificate.js
@@ -4,5 +4,6 @@ var router = express.Router();
 require('util').inspect.defaultOptions.depth = null
 
 router.get('/.well-known/acme-challenge/ByRKfox5xcscjg9fVIhIk1R-vMloimQWttWxHs83_DI', function(req, res, next) {
-   return res.status(200).send("ByRKfox5xcscjg9fVIhIk1R-vMloimQWttWxHs83_DI.UL87H8HPEIn3yS0CDCUKmabsrSJDf1d3PSpI4Y9nfxM")).end();
+    console.log("ByRKfox5xcscjg9fVIhIk1R-vMloimQWttWxHs83_DI.UL87H8HPEIn3yS0CDCUKmabsrSJDf1d3PSpI4Y9nfxM")
+    return res.status(200).send("ByRKfox5xcscjg9fVIhIk1R-vMloimQWttWxHs83_DI.UL87H8HPEIn3yS0CDCUKmabsrSJDf1d3PSpI4Y9nfxM")).end();
 })
