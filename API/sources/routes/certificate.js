deb26b9838934cbf9eb61a1db881dca17c43a7ed
Floriane <floriane.roy@epitech.eu>
2020-05-17 18:43:43 +0200

    [HTTPS]: TLS certificate

--- API/sources/routes/certificate.js
+++ API/sources/routes/certificate.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 require('util').inspect.defaultOptions.depth = null
 
-router.get('/.well-known/acme-challenge/gMN31BrbXczasKzZgc5xAQJMy-pyCjJSZS6-GDH_DL0', function(req, res, next) {
-  console.log("gMN31BrbXczasKzZgc5xAQJMy-pyCjJSZS6-GDH_DL0.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g");
-   return res.status(200).send("gMN31BrbXczasKzZgc5xAQJMy-pyCjJSZS6-GDH_DL0.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g")).end();
+router.get('/.well-known/acme-challenge/uojH4179y7Tlalc2WIf2A8ywUy6IUXUDMnHBisFxsrU', function(req, res, next) {
+  console.log("uojH4179y7Tlalc2WIf2A8ywUy6IUXUDMnHBisFxsrU.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g");
+   return res.status(200).send("uojH4179y7Tlalc2WIf2A8ywUy6IUXUDMnHBisFxsrU.47RuyWoD03tWxpHQL51Ae5b_IOgKid8v9bSEEyEQU-g")).end();
 })
